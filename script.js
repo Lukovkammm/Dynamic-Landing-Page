6f32ac1ea77c64ed10dbdd0695f602b41e46ff5b
Marina <artyusmarina@yandex.ru>
2021-01-22 14:45:06 +0300

    fix: change layout

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const yourName = document.querySelector('.name');
 const focus = document.querySelector('.enter_focus');
 
 window.onload = () => {
-    setInterval(showTime, 500);
+    setInterval(showTime, 1000);
     getName();
     getFocus();
 }
@@ -17,34 +17,56 @@ function showTime() {
     let seconds = date.getSeconds();
     const amPm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12 || 12;
-    time.innerHTML = `${hours}:${addZero(minutes)}:${addZero(seconds)} ${amPm}`;
+    time.textContent = `${hours}:${addZero(minutes)}:${addZero(seconds)} ${amPm}`;
 }
 
 function addZero(i) {
     return i < 10 ? "0" + i : i;
 }
 
+const content = {
+    morning: {
+        greeting: 'Good Morning, ',
+        image: "url('https://images.unsplash.com/photo-1420136390439-1482fc2ce4b9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80')",
+        color: null,
+    },
+    afternoon: {
+        greeting: 'Good Afternoon, ',
+        image: "url('https://images.unsplash.com/photo-1520220543854-138736ffc914?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80')",
+        color: null,
+    },
+    evening: {
+        greeting: 'Good Evening, ',
+        image: "url('https://images.unsplash.com/photo-1527261655500-59c476c630c2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80')",
+        color: '#fff',
+    },
+    night: {
+        greeting: 'Good Night, ',
+        image: "url('https://images.unsplash.com/photo-1475198751283-d3f5d231c7f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')",
+        color: '#fff',
+    }
+};
+
 function changeBackground(hours) {
     if (hours < 12) {
-        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1420136390439-1482fc2ce4b9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80')";
-        greeting.innerHTML = 'Good Morning, ';
+        document.body.style.backgroundImage = content.morning.image;
+        greeting.textContent = content.morning.greeting;
     } else if (hours < 18) {
-        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1520220543854-138736ffc914?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80')";
-        greeting.innerHTML = 'Good Afternoon, ';
+        document.body.style.backgroundImage = content.afternoon.image;
+        greeting.textContent = content.afternoon.greeting;
     } else if (hours < 22) {
-        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1527261655500-59c476c630c2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80')";
-        document.body.style.color = 'white';
-        greeting.innerHTML = 'Good Evening, ';
+        document.body.style.backgroundImage = content.evening.image;
+        document.body.style.color = content.evening.color;
+        greeting.textContent = content.evening.greeting;
     } else {
-        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1475198751283-d3f5d231c7f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')";
-        document.body.style.color = 'white';
-        greeting.innerHTML = 'Good Night, ';
+        document.body.style.backgroundImage = content.night.image;
+        document.body.style.color = content.night.color;
+        greeting.textContent = content.night.greeting;
     }
 }
 
 function getName() {
     if (localStorage.getItem('name') === null) {
-        yourName.textContent = '[Enter name]';
         yourName.focus();
     } else {
         yourName.textContent = localStorage.getItem('name');
@@ -53,18 +75,17 @@ function getName() {
 
 function setName(e) {
     if (e.type === 'keypress') {
-        if (e.keyCode === 13) {
-            localStorage.setItem('name', e.target.innerText);
+        if (e.keyCode === 17) {
+            localStorage.setItem('name', e.target.textContent);
             yourName.blur();
         }
     } else {
-        localStorage.setItem('name', e.target.innerText);
+        localStorage.setItem('name', e.target.textContent);
     }
 }
 
 function getFocus() {
     if (localStorage.getItem('focus') === null) {
-        focus.textContent = '[Enter focus]';
     } else {
         focus.textContent = localStorage.getItem('focus');
     }
@@ -73,11 +94,11 @@ function getFocus() {
 function setFocus(e) {
     if (e.type === 'keypress') {
         if (e.keyCode === 13) {
-            localStorage.setItem('focus', e.target.innerText);
+            localStorage.setItem('focus', e.target.textContent);
             focus.blur();
         }
     } else {
-        localStorage.setItem('focus', e.target.innerText);
+        localStorage.setItem('focus', e.target.textContent);
     }
 }
 
